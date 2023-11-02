4c63bbb5156914eb7804d706f610b079163d5b02
Giorgi Tsereteli <giorgi.webdev@gmail.com>
2023-11-01 23:45:01 -0400

    Refactor solution for passwordMaker.js

--- 7-kyu/Javascript/passwordMaker.js
+++ 7-kyu/Javascript/passwordMaker.js
@@ -1,41 +1,45 @@
+// 7kyu
+// Password Maker
+// https://www.codewars.com/kata/5637b03c6be7e01d99000046
+
+// DESCRIPTION:
+// One suggestion to build a satisfactory password is to start with a memorable phrase or sentence and 
+// make a password by extracting the first letter of each word.
+
+// Even better is to replace some of those letters with numbers 
+// (e.g., the letter O can be replaced with the number 0):
+
+// instead of including i or I put the number 1 in the password;
+// instead of including o or O put the number 0 in the password;
+// instead of including s or S put the number 5 in the password.
+
+// Examples:
+// "Give me liberty or give me death"  --> "Gml0gmd"
+// "Keep Calm and Carry On"            --> "KCaC0"
+
+// Tags: slice()
+
 /**
-Password Maker
-https://www.codewars.com/kata/5637b03c6be7e01d99000046
-
-DESCRIPTION:
-One suggestion to build a satisfactory password is to start with a memorable phrase or sentence and 
-make a password by extracting the first letter of each word.
-
-Even better is to replace some of those letters with numbers (e.g., the letter O can be replaced with the number 0):
-
-instead of including i or I put the number 1 in the password;
-instead of including o or O put the number 0 in the password;
-instead of including s or S put the number 5 in the password.
-
-Examples:
-"Give me liberty or give me death"  --> "Gml0gmd"
-"Keep Calm and Carry On"            --> "KCaC0"
-*/
-
+ * Convert given string into alphanumerical password
+ * 
+ * @param {string} phrase is the input string
+ * 
+ * @returns password with modified values
+ */
 function makePassword(phrase) {
-  let one = 'Ii';
-  let zero = 'oO';
-  let five = 'sS';
-
-  let result = '';
-
+  let password = '';
   let wordArr = phrase.split(' ');
-  for (let i = 0; i < wordArr.length; i++) {
-    const char = wordArr[i].slice(0, 1);
-    if (one.includes(char)) {
-      result += '1';
-    } else if (zero.includes(char)) {
-      result += '0';
-    } else if (five.includes(char)) {
-      result += '5';
+  for (let word of wordArr) {
+    const char = word.slice(0, 1).toLowerCase(); //First char of every word
+    if (char === 'i') {
+      password += '1';
+    } else if (char === 'o') {
+      password += '0';
+    } else if (char === 's') {
+      password += '5';
     } else {
-      result += char;
+      password += word.slice(0, 1);
     }
   }
-  return result;
+  return password;
 }
