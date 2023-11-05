f7b5a97a3174b42a8e821fc812876005cbf0ff7a
Giorgi Tsereteli <giorgi.webdev@gmail.com>
2023-11-04 20:22:14 -0400

    Refactor findTheCapitals.js

--- 7-kyu/Javascript/findTheCapitals.js
+++ 7-kyu/Javascript/findTheCapitals.js
@@ -1,19 +1,46 @@
+// 7kyu
+// Find the capitals
+// https://www.codewars.com/kata/539ee3b6757843632d00026b
+
+// DESCRIPTION:
+// Write a function that takes a single string (word) as argument. 
+// The function must return an ordered list containing the indexes of all capital letters in the string.
+
+// Example
+// Test.assertSimilar( capitals('CodEWaRs'), [0,3,4,6] );
+
+// Functions: forEach()
+
 /**
-DESCRIPTION:
-Write a function that takes a single string (word) as argument. 
-The function must return an ordered list containing the indexes of all capital letters in the string.
-
-Example
-Test.assertSimilar( capitals('CodEWaRs'), [0,3,4,6] );
-*/
-
+ * Creates array of indexes of each uppercase character
+ * 
+ * @param {string} word is the input string
+ * 
+ * @returns {array} of uppercase char indexes
+ */
 var capitals = function (word) {
   let result = [];
-  let charArr = word.split('');
-  charArr.forEach((char, index) => {
-    if (char == char.toUpperCase()) {
-      result.push(index);
+  word.split('').forEach((char, i) => {
+    if (char === char.toUpperCase()) {
+      result.push(i);
     }
   });
+  return result;
+};
+
+/**
+ * Second version of previous function
+ * 
+ * @param {string} word is the input string
+ * 
+ * @returns {array} of uppercase char indexes
+ */
+var capitals2 = function (word) {
+  let result = [];
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === word[i].toUpperCase()) {
+      result.push(i);
+    }
+  }
   return result;
 };
